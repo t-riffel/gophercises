1d0bcae56618d41c50659a97b1f62f7810f22a51
Todd Riffel <todd.riffel@linux.intel.com>
2018-03-25 12:56:36 -0500

    completed part 1 of exercise

--- quiz/main.go
+++ quiz/main.go
@@ -1,14 +1,67 @@
 package main
 
 import (
-	"flag"
+	"bufio"
+	"encoding/csv"
+	"fmt"
+	"io"
 	"os"
+	"strings"
 )
 
 func main() {
 	// quiz problems passed in through command-line
 	args := os.Args[1:]
-	namePtr := flag.String("file", "problems.csv", "filename")
-	flag.Parse()
 
+	// Open the file and parse quiz questions, maybe use a map
+	file, err := os.Open(args[0])
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	defer file.Close()
+
+	c := csv.NewReader(file)
+
+	c.Comma = ';'
+	score := 0
+	count := 0
+	var questions, answers [100]string
+
+	for {
+		// Read a single record which is one line
+		record, err := c.Read()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+
+		// Split the record
+		s := strings.Split(record[0], ",")
+		q, a := s[0], s[1]
+
+		// Store the question/answer
+		questions[count] = q
+		answers[count] = a
+		count++
+	}
+
+	for i := 0; i < count; i++ {
+		// Print question
+		fmt.Println("What is", questions[i], "?")
+
+		// Wait for answer
+		r := bufio.NewReader(os.Stdin)
+		text, _ := r.ReadString('\n')
+
+		// Check if input matches answer
+		if strings.TrimRight(text, "\n") == answers[i] {
+			score++
+		}
+	}
+
+	// Print final score
+	fmt.Println("Final Score:", score, "/", count)
 }
